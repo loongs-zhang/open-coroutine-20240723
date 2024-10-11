70f9dca49304795cc35130441e93e5d0d50f7202
dragon-zhang <zhangzicheng@apache.org>
2024-10-11 20:16:47 +0800

    disable test in windows

--- open-coroutine/tests/file_not_co.rs
+++ open-coroutine/tests/file_not_co.rs
@@ -1,5 +1,7 @@
 include!("../examples/file_not_co.rs");
 
+// The implementation of rust std is inconsistent between unix and windows.
+#[cfg(not(windows))]
 #[test]
 fn file_not_co() -> std::io::Result<()> {
     main()
